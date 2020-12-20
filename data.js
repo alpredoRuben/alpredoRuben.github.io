d5db5e5916af10750e08b509a585c72bbe3f7c4f
Ruben Alpredo <alpredo.tampubolon@gmail.com>
2020-12-21 02:16:56 +0700

    Upgrade CV

---
+++ data.js
@@ -0,0 +1,28 @@
+var experienceItems = [
+    {from:'Mar 2019', to:'Mei 2020', company: 'PT. MINDA PERDANA INDONESIA', position: 'Software Engineer', location: 'Bekasi', status: 'Remote'},
+    {from:'Agu 2018', to:'Nov 2018', company: 'PT. ELOKET SUKSES ABADI', position: 'Backend Developer', location: 'Medan', status: ''},
+    {from:'Okt 2017', to:'Agu 2018', company: 'PT. INTERPRIMA NUSANTARA MANDIRI', position: 'Software Engineer', location: 'Medan', status: ''},
+    {from:'Mei 2017', to:'Jun 2017', company: 'PT. SANDRA PRIMA', position: 'Freelance Programmer', location: 'Medan', status: 'Remote'},
+    {from:'Feb 2015', to:'Mar 2015', company: 'PT. INDONESIA ASAHAN ALUMINIUM (PERSERO)', position: 'Freelance Programmer', location: 'Medan', status: 'Remote'},
+    {from:'Okt 2011', to:'Jul 2013', company: 'PT. SUMATERA COMPUTER CENTER', position: 'IT Support', location: 'Medan', status: ''},
+];
+
+
+var skillItem = [
+    [
+        {id:2, name: "Laravel Framework", point: 90},
+        {id:3, name: "Express JS", point: 80},
+        {id:4, name: "React JS", point: 80},
+        {id:5, name: "JQuery", point: 90},
+        {id:6, name: "MySQL", point: 85},
+        {id:7, name: "Postgree SQL", point: 80},
+    ],
+    [
+        {id:8, name: "Mongo DB", point: 70},
+        {id:9, name: "Redis", point: 75},
+        {id:10, name: "Linux", point: 70},
+        {id:11, name: "Windows", point: 90},
+        {id:12, name: "Git", point: 85},
+        {id:13, name: "Microsoft Office", point: 90},
+    ]
+];
